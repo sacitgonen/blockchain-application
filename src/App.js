f808a0fb744d1274f20ce8a11e2cb4a85a7ad9dc
sacit.gonen <sacit.gonen@tubitak.gov.tr>
2022-07-27 14:02:32 +0300

    Context api öncesi commitimiz

--- src/App.js
+++ src/App.js
@@ -1,77 +1,58 @@
 import React, {useState} from "react";
-import {sha256} from "js-sha256";
 import Block from "./components/Block";
 import './App.css';
 
 function App() {
+    const [blockList, setBlockList] = useState([
+        {
+            blockNumber: 0,
+            nonce: 0,
+            preHash: "0",
+            hash: "0"
+        },
+        {
+            blockNumber: 1,
+            nonce: 1,
+            preHash: "1",
+            hash: "1"
+        },
+        {
+            blockNumber: 2,
+            nonce: 2,
+            preHash: "2",
+            hash: "2"
+        },
+        {
+            blockNumber: 3,
+            nonce: 3,
+            preHash: "3",
+            hash: "3"
+        }
+    ]);
 
-    const [blockNumber0, setBlockNumber0] = useState();
-    const [nonce0, setNonce0] = useState();
-    const [data0, setData0] = useState("");
-    const [preHash0, setPreHash0] = useState();
-    const [hash0, setHash0] = useState();
-
-    const [blockNumber1, setBlockNumber1] = useState();
-    const [nonce1, setNonce1] = useState();
-    const [data1, setData1] = useState("");
-    const [preHash1, setPreHash1] = useState();
-    const [hash1, setHash1] = useState();
-
-    const [blockNumber2, setBlockNumber2] = useState();
-    const [nonce2, setNonce2] = useState();
-    const [data2, setData2] = useState("");
-    const [preHash2, setPreHash2] = useState();
-    const [hash2, setHash2] = useState();
-
-    const [blockNumber3, setBlockNumber3] = useState();
-    const [nonce3, setNonce3] = useState();
-    const [data3, setData3] = useState("");
-    const [preHash3, setPreHash3] = useState();
-    const [hash3, setHash3] = useState();
-
+    const updateBlockList = (index,newHash) => {
+        const arrCopy = [...blockList];
+        arrCopy[index].hash = newHash;
+        if(index < blockList.length - 1){
+            arrCopy[index+1].preHash = newHash;
+        }
+        setBlockList(arrCopy);
+    };
 
     return (
-        <div>
-            <Block
-                blockNumber={blockNumber0}
-                nonce={nonce0}
-                data={data0}
-                preHash={preHash0}
-                hash={hash0}
-                setData={setData0}
-                setHash={setHash0}
-                setPreHash={setPreHash1}
-            />
-            <Block
-                blockNumber={blockNumber1}
-                nonce={nonce1}
-                data={data1}
-                preHash={preHash1}
-                hash={hash1}
-                setData={setData1}
-                setHash={setHash1}
-                setPreHash={setPreHash2}
-            />
-            <Block
-                blockNumber={blockNumber2}
-                nonce={nonce2}
-                data={data2}
-                preHash={preHash2}
-                hash={hash2}
-                setData={setData2}
-                setHash={setHash2}
-                setPreHash={setPreHash3}
-            />
-            <Block
-                blockNumber={blockNumber3}
-                nonce={nonce3}
-                data={data3}
-                preHash={preHash3}
-                hash={hash3}
-                setData={setData3}
-                setHash={setHash3}
-                setPreHash={setPreHash0}
-            />
+        <div className="App">
+            {
+                blockList.map((block) => {
+                    return (
+                        <Block key={block.blockNumber}
+                               blockNumber={block.blockNumber}
+                               preHash={block.preHash}
+                               hash={block.hash}
+                               updateBlockList={updateBlockList}
+                        />
+                    )
+                })
+            }
         </div>
     );
 }
--- src/components/Block.js
+++ src/components/Block.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {sha256} from "js-sha256";
+import PropTypes from "prop-types";
 
 function Block(props) {
+
+    const [data, setData] = useState("");
+    const {blockNumber, preHash, hash, updateBlockList} = props;
+
+    const handleOnChange = (event) => {
+        setData(event.target.value);
+    };
+
+    useEffect(() => {
+        updateBlockList(blockNumber,sha256(`${data}${preHash}`));
+    },[preHash,data]);
+
     return (
-        <div className="block">
-            block number: <input value={props.blockNumber} readOnly={true}/><br/>
-            nonce: <input  value={props.nonce} readOnly={true}/><br/>
-            data: <input value={props.data} onChange={event => {
-            props.setData(event.target.value);
-            props.setHash(sha256(event.target.value));
-            props.setPreHash(sha256(event.target.value))
-            }}/><br/>
-            preHash: <input value={props.preHash} readOnly={true}/><br/>
-            hash: <input value={props.hash} readOnly={true}/><br/>
+        <div className="Block">
+            <form>
+                <h2>Block {blockNumber}</h2>
+
+                <label>data: </label>
+                <input value={data} onChange={handleOnChange}/>
+                <br/>
+                <label>nonce: </label>
+                <input value={"props.nonce"} readOnly={true}/>
+                <br/>
+                <label>preHash: </label>
+                <input value={preHash} readOnly={true}/>
+                <br/>
+                <label>hash: </label>
+                <input value={hash} readOnly={true}/>
+                <br/>
+            </form>
         </div>
     );
 }
 
 export default Block;
+
+Block.defaultProps = {
+    blockNumber: "-",
+    preHash: "-",
+    hash: "-",
+    updateBlockList: null
+};
+
+Block.propTypes = {
+    blockNumber: PropTypes.number,
+    preHash: PropTypes.string,
+    hash: PropTypes.string,
+    updateBlockList: PropTypes.func
+};
